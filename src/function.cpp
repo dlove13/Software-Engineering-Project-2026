d7174ae062a34d32353807a356c0950e07e083dd
dlove13 <dlove13@ku.edu>
2026-04-21 14:53:11 -0500

    create makefile, main, and function header files. started display prototype and added TODOs

---
+++ src/function.cpp
@@ -0,0 +1,32 @@
+#include "function.h"
+
+
+void Display() {
+
+    Display_Menu();
+}
+
+void Display_Menu () {
+        
+        cout << "Display is running..." << endl;
+        
+        cout << "\n0) Exit Program " << endl;
+        cout << "1) Execute Arithmetic Expression Evaluator (Not Implemented)" << endl;
+        cout << "2) Run Unit Tests" << endl;
+        cout << "\nEnter 0, 1, or 2 ";
+
+}
+
+int userMenuChoice() {
+    return 0;
+}
+
+//vector<char> Tokenizer () {}
+/*
+   vector<char> Parser () {}
+
+   vector<char> Evaluator () {}
+
+   int Error_Handler () {}
+
+*/
