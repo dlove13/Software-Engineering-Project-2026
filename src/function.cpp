f84d1149cf24012beb19fcf8cd181a5c9fde7cde
dlove13 <dlove13@ku.edu>
2026-04-24 18:15:38 -0500

    parser logic WIP

--- src/function.cpp
+++ src/function.cpp
@@ -28,13 +28,110 @@ char userMenuChoice() {
     return inputBuf;
     }
 
+class Token {
+
+    public:
+        char token;
+        int precedence;
+        char associativity;
+};
 
 //vector<char> Tokenizer () {}
-/*
-   vector<char> Parser () {}
+vector<char> Parser (vector<Token> tokenizedInput) {
+   /*   Implement with Shunting Yard Algorithm
+    *
+    *   Numbers -> directly into output queue
+    *   Left Parenthesis -> Push() onto operator stack
+    *   Right parenthesis -> Pop() operators from stack and add to output
+    *       until left parenthesis is reached
+    *
+    *       Discard both parenthesis
+    *
+    *   Operators -> Compare current operator precedence and associativity to
+    *       operator at the top of the stack
+    *
+    *       if curr.precedence >= top.precendence and associativity == 'L':
+    *           Pop() to output queue before pushing new operator
+    *
+    *   Once Input == Empty: Pop() remaining operators from stack and add to
+    *       input
+    *
+    * */ 
+   
+    //Init containers
+    stack<Token> operatorStack; 
+    vector<char> postFixExpr;
+    
+    for (int i = 0; i < tokenizedInput.size(); i++)
+    {
+        //Check if number, then push to output
+        if (isidigit(tokenizedInput.at(i).token) 
+        {
+            postFixExpr.push_back(tokenizedInput.at(i).token;
+        }
+        //Push left parenthesis onto operator stack
+        else if (tokenizedInput.at(i).token == '(')
+        {
+            operatorStack.push(tokenizedInput.at(i).token;
+        }
+        //Pop operators from stack and add to output until next left parenthesis
+        else if (tokenizedInput.at(i).token == ')')
+        {
+            while (!operatorStack.empty() && operatorStack.top() != '(')
+            {
+                postFixExpr.push_back(operatorStack.top().token);
+                operatorStack.pop()
+            }
+            
+            if (!operatorStack.empty()) 
+            {
+                operatorStack.pop();
+            } 
+            else 
+            {
+                //Error-Handling: Mismatched Parenthesis
+            }
+            
+            //Pop remaining parenthesis
+                   }
 
-   vector<char> Evaluator () {}
+        else if (tokenizedInput.at(i).token == '+' ||
+                 tokenizedInput.at(i).token == '-' ||      
+                 tokenizedInput.at(i).token == '/' ||
+                 tokenizedInput.at(i).token == '*' ||
+            )
+        {
+            while (!operatorStack.empty() &&
+                    operatorStack.top().token != '(' &&
+                    operatorStack.top().precedence >= tokenizedInput.at(i).precedence &&
+                    tokenizedInput.at(i).associativity == 'L')
+            {
+                postFixExpr.push_back(operatorStack.top().token);
+                operatorStack.pop();
+                operatorStack.push(tokenizedInput.at(i).token);
+            }
+                operatorStack.push(tokenizedInput.at(i).token);
+            }
+        }
+    }
 
-   int Error_Handler () {}
+    //Empty out remaining operators
+    while (!operatorStack.empty())
+    {
+        if (operatorStack.top().token == '(')
+        {
+            //Error_Handler: Mismatched Parenthesis
+        }
 
-*/
+        postFixExpr.push_back(operatorStack.top().token);
+        operatorStack.pop();
+    }
+
+    return postFixExpr;
+}
+
+//   vector<char> Evaluator () {}
+
+// int Error_Handler () {}
+
+
--- src/function.h
+++ src/function.h
@@ -2,16 +2,18 @@
 #include <vector>
 #include <cstdlib>
 #include <cctype>
+#include <stack>
 using namespace std;
 
 void Display();
 void Display_Menu();
 char userMenuChoice();   //TODO: Check for valid input
 
-/*
-vector<char> Tokenizer();   //TODO
-vector<char> Parser();      //TODO
-int Evalutor();             //TODO
+class Token;
 
-int Error_Handler();        //TODO
-*/
+//vector<char> Tokenizer();   //TODO
+vector<char> Parser( vector<Token> tokenizedInput);      //TODO
+//int Evalutor();             //TODO
+
+//int Error_Handler();        //TODO
+
