7411fe7a343929008739e0742a07e9b49297fa60
dlove13 <dlove13@ku.edu>
2026-04-30 15:10:15 -0500

    WIP Unit Test framework - Parser

--- src/unit_tests.cpp
+++ src/unit_tests.cpp
@@ -2,30 +2,102 @@
 
 /*Define unit tests for each individual t*/
 
-//Parser Test
+//Infix Expression
 vector<char> test_expression_1 = {'3', '+', '4'};
-vector<char> test_expression_2 = {}
-vector<char> test_expression_3 = {}
-vector<char> test_expression_4 = {}
-vector<char> test_expression_5 = {}
+vector<char> test_expression_2 = {'8' , '-', '(', '5', '-', '2', ')'};
+vector<char> test_expression_3 = {'-','8', '+', '(', '2', '*', '3', ')'};
+vector<char> test_expression_4 = {'4', '*', '(', '3', '+', '2', ')', '%', '7', '-', '1'};
 
-
+//Crude Tokenizer
 vector<Token> parser_test_1;
+vector<Token> parser_test_2;
+vector<Token> parser_test_3;
+vector<Token> parser_test_4;
+
+//Expected Output
+vector<char> expected_1 = {'3','4','+'};
+vector<char> expected_2 = {'8','5','2', '-', '-'};
+vector<char> expected_3 = {'-','8', '2', '3', '*', '+'};
+vector<char> expected_4 = {'4','3','2','+','*','7','%', '1', '-'};
+
+
+//Actual output
+vector<char> test_rpn_1;
+vector<char> test_rpn_2;
+vector<char> test_rpn_3;
+vector<char> test_rpn_4;
+
+
+
+vector<Token> CreateTokenList(vector<char> expr)
+{
+    vector<Token> tokens;
+
+    for (size_t i = 0; i < expr.size(); i++){
+        Token newToken;
+        newToken.token = expr.at(i);
+
+        tokens.push_back(newToken);
+    }
+    
+    return tokens;
+}
+
+void PrintTokens(vector<Token> tokenlist)
+{
+    //Test Print vector
+    for (size_t i = 0; i < tokenlist.size(); i++)
+    {
+        cout << tokenlist.at(i).token;
+    }
+
+    cout << endl;
+}
+
+void PrintRPN(vector<char> parsedExpression)
+{
+
+    for (size_t i = 0; i < parsedExpression.size(); i++)
+    {
+        cout << parsedExpression.at(i);
+    
+    }
+
+    cout << endl;
+}
+
+void Result (vector<char> );
 
 void UnitTests ()
 {
-    for (size_t i = 0; i < test_expression_1.size(); i++){
-        Token newToken;
-        newToken.token = test_expression_1.at(i);
+    //Init list of tokens
+    parser_test_1 = CreateTokenList(test_expression_1);
+    parser_test_2 = CreateTokenList(test_expression_2);
+    parser_test_3 = CreateTokenList(test_expression_3);
+    parser_test_4 = CreateTokenList(test_expression_4);
+    
 
-        parser_test_1.push_back(newToken);
-    }
+    //Print Infix expressions
+    cout << "Infix Expressions" << endl;
+    cout << "-----------------" << endl;
+    PrintTokens(parser_test_1);
+    PrintTokens(parser_test_2);
+    PrintTokens(parser_test_3);
+    PrintTokens(parser_test_4);
+    
+    //Parse expressions
+    test_rpn_1 = Parser(parser_test_1);
+    test_rpn_2 = Parser(parser_test_2);
+    test_rpn_3 = Parser(parser_test_3);
+    test_rpn_4 = Parser(parser_test_4);
 
-    //Test Print vector
-    for (size_t i = 0; i < parser_test_1.size(); i++)
-    {
-        cout << parser_test_1.at(i).token << endl;
-    }
 
+    //Print RPN expressions
+    cout << "\nRPN Expression" << endl;
+    cout << "--------------" << endl;
+    PrintRPN(test_rpn_1);
+    PrintRPN(test_rpn_2);
+    PrintRPN(test_rpn_3);
+    PrintRPN(test_rpn_4);
 }
 
